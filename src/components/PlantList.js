81f174335f4df734893f57ef4eb8e625d3086a78
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-22 17:34:09 -0400

    core done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,23 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, search}) {
+  
+  const filPlants = plants.filter((plant)=>{
+    return plant.name.includes(search)
+  })
+  
+
+  const cardOfPlant = filPlants.map((plant)=>{
+    return <PlantCard 
+    key={plant.id} 
+    image={plant.image} 
+    name={plant.name} 
+    id={plant.id} 
+    price={plant.price} />
+  })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{cardOfPlant}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchCon, setSearchCon] = useState("")
+  
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then(r=>r.json())
+    .then(data=>setPlants(data))
+  },[])
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants}/>
+      <Search setSearchCon={setSearchCon}/>
+      <PlantList search={searchCon} plants={plants} />
     </main>
   );
 }
